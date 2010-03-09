d8cfee080697295a94e306620f12ef5886815391
John Hawthorn <john.hawthorn@gmail.com>
2010-03-08 21:35:46 -0800

    added javascript to lookup via isbn

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function ajax_response(response){
+  attrs = response.responseJSON;
+}
+
+function lookup(form){
+  isbn = form.book_isbn.value;
+  new Ajax.Request("lookup.json?isbn="+isbn, {method: 'get', onComplete: (function(response){
+          attrs = response.responseJSON;
+          form.book_title.value     = attrs.title;
+          form.book_summary.value   = attrs.summary;
+          form.book_author.value    = attrs.author;
+          form.book_publisher.value = attrs.publisher;
+          form.book_edition.value   = attrs.edition;
+        }
+        )});
+};
+
